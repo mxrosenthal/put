3ad5575eabe52aa945c1ca1c28ae506c18e5d061
Max Rosenthal <maxrosenthal@Maxs-MacBook-Pro.local>
2025-02-26 13:45:19 -0800

    updated drag and drop to say what file types we accept

--- src/photo-updating-tool/components/DragDropPhoto.tsx
+++ src/photo-updating-tool/components/DragDropPhoto.tsx
@@ -2,19 +2,34 @@ import { css } from "@emotion/css";
 
 interface DragDropPhotoProps {
   setSelectedImage: React.Dispatch<React.SetStateAction<string | null>>;
+  setImageSize: React.Dispatch<
+    React.SetStateAction<{
+      width: number;
+      height: number;
+    } | null>
+  >;
 }
 
 export function DragDropPhoto(props: DragDropPhotoProps) {
-  const { setSelectedImage } = props;
+  const { setSelectedImage, setImageSize } = props;
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
       const file = event.target.files[0];
       const reader = new FileReader();
+
       reader.onload = (e) => {
         if (e.target?.result) {
+          const img = new Image();
+          img.src = e.target.result as string;
+
+          img.onload = () => {
+            setImageSize({ width: img.width, height: img.height });
+          };
+
           setSelectedImage(e.target.result as string);
         }
       };
+
       reader.readAsDataURL(file);
     }
   };
@@ -35,29 +50,35 @@ export function DragDropPhoto(props: DragDropPhotoProps) {
   };
 
   return (
-    <div
-      onDragOver={(e) => e.preventDefault()}
-      onDrop={handleDrop}
-      className={styles.dragDrop}
-    >
-      <p>Drag and drop to Put. a photo here, or click to select one</p>
+    <div className={styles.container}>
+      <div
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={handleDrop}
+        className={styles.dragDrop}
+      >
+        <p>Drag and drop to Put. a photo here, or click to select one</p>
 
-      <input
-        type='file'
-        accept='image/*'
-        onChange={handleFileChange}
-        style={{ display: "none" }}
-        id='fileInput'
-      />
-      <label htmlFor='fileInput' className={styles.label}>
-        Please Put. Photo
-      </label>
-      <p>(Supports JPEG, PNG, GIF, BMP, and WebP)</p>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleFileChange}
+          style={{ display: "none" }}
+          id='fileInput'
+        />
+        <label htmlFor='fileInput' className={styles.label}>
+          Please Put. Photo
+        </label>
+        <p>(Supports JPEG, PNG, GIF, BMP, and WebP)</p>
+      </div>
     </div>
   );
 }
 
 const styles = {
+  container: css({
+    display: "flex",
+    justifyContent: "center",
+  }),
   label: css({
     padding: "10px 20px",
     backgroundColor: "#007bff",
@@ -68,7 +89,7 @@ const styles = {
   }),
   dragDrop: css({
     width: "98%",
-    height: "400px",
+    height: "90vh",
     border: "2px dashed #ccc",
     display: "flex",
     alignItems: "center",
